2f430aaf071d70466c66d0029ab534145f568434
Neal JIn Oh <no4259@gmail.com>
2019-09-06 05:41:18 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, gender) VALUES ("Mr.Chocolate", 20, "male");
+INSERT INTO bears (name, age, gender) VALUES ("Rowdy", 14, "female");
+INSERT INTO bears (name, age, gender) VALUES ("jsmine", 24, "female");
+INSERT INTO bears (name, age, gender) VALUES ("Max", 14, "male");
+INSERT INTO bears (name, age, gender) VALUES ("jess", 45, "female");
+INSERT INTO bears (name, age, gender) VALUES ("tom", 23, "male");
+INSERT INTO bears (name, age, gender) VALUES ("mason", 15, "male");
+INSERT INTO bears (name, age, gender) VALUES (null, 35, "male");
+ 
+ 
